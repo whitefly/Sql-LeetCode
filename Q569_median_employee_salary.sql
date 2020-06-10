bff09f29589ce6acf247f9a5fe7ceea0584d55ff
zhouang <316447676@qq.com>
2020-06-10 17:37:21 +0800

    569 组内中位数问题

--- Q569_median_employee_salary.sql
+++
@@ -1,9 +0,0 @@
-select any_value(e1.Id)as id ,e1.Company, e1.Salary
-from Employee1 e1,
-     Employee1 e2
-where e1.Company = e2.Company
-group by e1.Company, e1.Salary
-having sum(if(e1.Salary = e2.Salary, 1, 0)) >= abs(sum(sign(e1.Salary - e2.Salary)))
-
-
-
